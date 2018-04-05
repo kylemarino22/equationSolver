6b21016b03ede33baad5ef96230b9cb6cb482306
Unknown <kyle.marino22@gmail.com>
2018-04-04 21:25:50 -0700

    Basic Idea description.

---
+++ src/Main.java
@@ -0,0 +1,45 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+    /*equation gets inputted as a string
+     "y = xˆ3 - 5x + 3"
+     Gets Parsed into correct format (Parser Class)
+     "y = xˆ3 - 5*x + 3"
+     Gets compiled into an equation object (Compiler Class)
+      - A list of steps for equation to be solved
+      - Finds innermost parentheses
+      - Works from right to left
+
+     Class eqOb{
+
+     Hashmap of functions:
+     a 6,x
+     d c,3
+     m c,4
+     e 4,c
+
+     functions:
+
+     e (a,b)
+     ln (a,b)
+     a (a,b)
+     s (a,b)
+     m (a,b)
+     d (a,b)
+     sin (a,b)
+     asin (a,b)
+     cos (a,b)
+     acos (a,b)
+     tan (a,b)
+     atan (a,b)
+     }
+
+
+
+
+        */
+	// write your code here
+        System.out.println("hello");
+    }
+}
